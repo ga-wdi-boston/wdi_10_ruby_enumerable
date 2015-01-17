2cc57e44dc91537b1634427e2f923b6b4092b190
Daniel Tyson <danielctyson@gmail.com>
2015-01-17 18:43:45 -0500

    fixed person with most experience method and added test.

--- lib/enum/nested.rb
+++ lib/enum/nested.rb
@@ -19,27 +19,28 @@ class People
   end
 
   def ages_sum
-    @people.map{ |person| person.age }.reduce(:+)
+    people.map{ |person| person.age }.reduce(:+)
   end
 
   def average_age
-    @people.map{ |person| person.age }.reduce(:+) / @people.length
+    people.map{ |person| person.age }.reduce(:+) / @people.length
   end
 
   def total_years_programming_experience_for_all_languages
-    @people.map{ |person| person.years_language_experience.values.reduce(:+) }.reduce(:+)
+    people.map{ |person| person.years_language_experience.values.reduce(:+) }.reduce(:+)
   end
 
   def favorite_food_frequency
-    @people.map { |person| person.favorite_foods.each_with_object(Hash.new(0)) {|food, count| count[food] += 1} }.inject{|memo, el| memo.merge(el){|k, old, new_v| old += new_v}}
+    people.map { |person| person.favorite_foods.each_with_object(Hash.new(0)) {|food, count| count[food] += 1} }.inject{|memo, el| memo.merge(el){|k, old, new_v| old += new_v}}
   end
 
   def total_combined_years_language_experience(language)
-    @people.map{ |person| person.years_language_experience[language]}.compact.reduce(:+)
+    people.map{ |person| person.years_language_experience[language]}.compact.reduce(:+)
   end
 
   def person_with_most_experience_in_language(language)
-    @people.each_with_object(Hash.new(0)){|person, years| years[person.name] = person.years_language_experience[language]}.delete_if{|k,v| v == nil}.max[0]
+    # binding.pry
+    people.each_with_object(Hash.new(0)){|person, years| years[person.name] = person.years_language_experience[language]}.delete_if{|k,v| v == nil}.max_by {|k,v| v}[0]
   end
 
   private
