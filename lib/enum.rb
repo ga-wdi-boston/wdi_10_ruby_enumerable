496001fe4be0fab98a620b75d21d3adc08af5a4d
J.Marx <jennyrutledge@gmail.com>
2015-01-17 13:07:32 -0500

    Add function to all_words_longer_than_lenght?, contains_a_world_longer_than?, capitalize_words, and reverse_words to MyEnumeration class.

--- lib/enum.rb
+++ lib/enum.rb
@@ -13,8 +13,26 @@ class MyEnumeration
     inArr.count
   end
 
-   def number_of_floats_or_fixnums
+  def number_of_floats_or_fixnums
      inArr.map{|elem| elem.is_a?(Numeric)}.count(true)
   end
+
+  def all_words_longer_than_length?(len)
+    inArr.all?{ |word| word.length > len}
+  end
+
+  def contains_a_word_longer_than?(len)
+    inArr.any?{ |word| word.length > len}
+  end
+
+  def capitalize_words
+    inArr.map(&:capitalize)
+  end
+
+  def reverse_words
+    inArr.map(&:reverse)
+  end
+
+
 
 end
