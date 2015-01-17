577e40dad3459e3674ca542e92b7b16aff8555cd
J.Marx <jennyrutledge@gmail.com>
2015-01-17 17:59:10 -0500

    Add index_of_1st_awesome_element, group_elements_by_favorite_language, sum_of_experiences, longest_element_name_using_inject, and most_experienced_element to MyEnumeration class.

--- lib/enum.rb
+++ lib/enum.rb
@@ -14,7 +14,15 @@ class MyEnumeration
   end
 
   def number_of_floats_or_fixnums
-     inArr.map{|elem| elem.is_a?(Numeric)}.count(true)
+    inArr.map{|elem| elem.is_a?(Numeric)}.count(true)
+  end
+
+  def sum_of_numeric_elements
+    inArr.select { |num| num.is_a? (Numeric) }.reduce(:+)
+  end
+
+  def product_of_numeric_elements
+    inArr.select { |num| num.is_a? (Numeric) }.reduce(:*)
   end
 
   def all_words_longer_than_length?(len)
@@ -40,7 +48,7 @@ class MyEnumeration
   def positive_and_negative_numbers
     inArr.flat_map{ |num| [num.abs, -num.abs] }
   end
-
+#11
   def find_first_awesome_person
     inArr.find{|stat| stat[:awesome] == true}
   end
@@ -48,5 +56,48 @@ class MyEnumeration
   def remove_first_three
     inArr.drop(3)
   end
+
+  def drop_until_its_hot
+    inArr.drop_while { |word| word != "hot" }
+  end
+
+  def groups_of(num)
+    inArr.each_slice(num).to_a
+  end
+
+  def element_frequency_count
+    inArr.each_with_object({}) do |key, freq_hash|
+      freq_hash[key] = inArr.count(key)
+    end
+  end
+#16
+  def elements_ending_in_er
+    inArr.find_all{|word| word.split("").last(2) == ["e","r"]}
+  end
+
+  def index_of_first_awesome_element
+    inArr.find_index{|info| info[:awesome]}
+  end
+
+  def group_elements_by_favorite_language
+    inArr.group_by{|person| person[:favorite_language]}
+  end
+
+  def sum_of_experiences
+    inArr.map{ | person | person[:years_experience] }.reduce(:+)
+  end
+
+  def longest_element_name_using_inject
+    inArr.inject{ |longest, info| longest[:name].length > info[:name].length ? longest : info}
+  end
+#19 failures
+  def most_experienced_element
+    inArr.max_by { |info| info[:years_experience] }
+  end
+  #18 failures
+  def element_present?(name)
+    inArr.include?(name)
+  end
+  #17 failures
 
 end
