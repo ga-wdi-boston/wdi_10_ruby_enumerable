ddc4229154c840d14680d466bd490702ee5d73e9
J.Marx <jennyrutledge@gmail.com>
2015-01-17 12:30:05 -0500

    Add number_of_elements_in_collection to MyEnumeration class.

--- lib/enum.rb
+++ lib/enum.rb
@@ -2,3 +2,18 @@ require 'enum/basic_enum'
 require 'enum/arrays'
 require 'enum/nested'
 
+class MyEnumeration
+
+  attr_reader :inArr
+  def initialize(inArr)
+    @inArr = inArr
+  end
+
+  def number_of_elements_in_collection
+    inArr.count
+  end
+
+  # def number_of_floats_or_fixnums
+  #   inArr
+
+end
