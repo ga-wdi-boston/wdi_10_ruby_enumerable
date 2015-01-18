2aef3031607abb07994f1f3eff97f70e2fc41f34
Dan Stoppelman <dstop75@gmail.com>
2015-01-17 22:18:21 -0500

    Refactoring

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,45 +1,33 @@
 class Array
 
-  def initialize(array)
-    @array = array
-  end
-
   def sum_of_numeric_elements
-    @array.select { |item| item.is_a? Numeric }.reduce(:+)
+    select { |item| item.is_a? Numeric }.reduce(:+)
   end
 
   def product_of_numeric_elements
-    @array.select { |item| item.is_a? Numeric }.reduce(:*)
+    select { |item| item.is_a? Numeric }.reduce(:*)
   end
 
   def even_numeric_elements
-    @array.select { |item| item.is_a? Fixnum }.select { |item| item.even? }
+    select { |item| item.is_a? Fixnum }.select(&:even?)
   end
 
   def odd_numeric_elements
-    @array.select { |item| item.is_a? Fixnum }.select { |item| item.odd? }
+    select { |item| item.is_a? Fixnum }.select(&:odd?)
   end
 
   def string_elements
-    @array.select { |item| item.is_a? String }
+    select { |item| item.is_a? String }
   end
 
   def lowercase_string_elements_as_uppercase
-    @array.select do |item|
-      item.is_a? String
-    end.select do | string|
-      string == string.downcase
-    end.map { |string| string.capitalize }
+    select { |item| item.is_a? String }
+    .select { |string| string == string.downcase }
+    .map { |string| string.capitalize }
   end
 
   def capitalized_elements
-    @array.select { |item| item.is_a? String }.select { |string| string[0] == string[0].upcase }
+    select { |item| item.is_a? String }
+    .select { |string| string[0] == string[0].upcase }
   end
-
-  private
-
-  def array
-    @array
-  end
-
 end
