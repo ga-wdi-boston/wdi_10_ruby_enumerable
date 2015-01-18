2284177cfbb9fc3660358bbbdde751202a3d7b9a
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-01-18 18:38:21 -0500

    Pass first test

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,12 +1,19 @@
 class Array
 
+  def initialize(my_array)
+     @my_array = my_array
+  end
+
   def sum_of_numeric_elements
+     @my_array.select{ |i| i.is_a? (Numeric)}.inject{ |number,sum| number + sum}.to_f
   end
 
   def product_of_numeric_elements
+    @my_array.select{ |i| Integer(i) rescue false}.inject{ |number,mult| number * mult}.to_i.to_f # question about this
   end
 
   def even_numeric_elements
+    @my_array.to_a.select{ |i| i.even? }
   end
 
   def odd_numeric_elements
