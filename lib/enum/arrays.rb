f3d192f900835159ee75e9bf9855f37fb41c8dfd
KBondanza <kelly.bondanza@gmail.com>
2015-01-18 17:51:54 -0500

    I think the Gemfile was updated Friday for the right ruby version. I finished half of lib/enum/arrays.rb

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,15 +1,19 @@
 class Array
 
-  def sum_of_numeric_elements
+  def sum_of_numeric_elements #using elem for elements.
+    select { [elem] elem.is_a? Numeric }.reduce(:+) #is_a? to return a boolean value, that if e is a numeric element then add them and return the sum.
   end
 
   def product_of_numeric_elements
+    select { [elem] elem.is_a? Numeric }.reduce(:*)
   end
 
   def even_numeric_elements
+    select { [elem] elem.is_? Numeric && (elem % 2 == 0)}
   end
 
   def odd_numeric_elements
+    select { [elem] elem.is_? Numeric && (elem % 2 !== 0)}
   end
 
   def string_elements
