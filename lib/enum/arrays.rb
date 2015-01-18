64c58e0f4eb343eed42ddc97aba0a9704959dca5
KBondanza <kelly.bondanza@gmail.com>
2015-01-18 18:17:46 -0500

    Finished 5 out of 7, however still can't run tests due to an unidentified syntax error; tried to add in pry bug that doesn't seem to load/help.

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,28 +1,35 @@
+require 'pry-byebug'
+
 class Array
 
-  def sum_of_numeric_elements #using elem for elements.
-    select { [elem] elem.is_a? Numeric }.reduce(:+) #is_a? to return a boolean value, that if e is a numeric element then add them and return the sum.
+  def sum_of_numeric_elements #using e for elements.
+    select { [e] e.is_a? Numeric }.reduce(:+) #is_a? to return a boolean value, that if e is a numeric element then add them and return the sum.
   end
 
   def product_of_numeric_elements
-    select { [elem] elem.is_a? Numeric }.reduce(:*)
+    select { [e] e.is_a? Numeric }.reduce(:*)
   end
 
   def even_numeric_elements
-    select { [elem] elem.is_? Numeric && (elem % 2 == 0)}
+    select { [e] e.is_a? Numeric && (elem % 2 == 0)}
   end
 
   def odd_numeric_elements
-    select { [elem] elem.is_? Numeric && (elem % 2 !== 0)}
+    select { [e] e.is_a? Numeric && (elem % 2 !== 0)}
   end
 
   def string_elements
+    select { [e] e.is_a? String }
   end
 
   def lowercase_string_elements_as_uppercase
+    select { [e] }.map(&:capitalize)
   end
 
   def capitalized_elements
+    select { [e] }
   end
 
 end
+
+binding.pry
