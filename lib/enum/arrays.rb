c6670995a08b72fd8980eb63a3a85389653ca046
Doug <djmacdonald@uchicago.edu>
2015-01-17 19:25:27 -0500

    Removed unneeded selfs.

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,31 +1,31 @@
 class Array
 
   def sum_of_numeric_elements
-    self.select { |el| el.is_a?(Numeric) }.inject(:+)
+    select { |el| el.is_a?(Numeric) }.inject(:+)
   end
 
   def product_of_numeric_elements
-    self.select { |el| el.is_a?(Numeric) }.inject(:*)
+    select { |el| el.is_a?(Numeric) }.inject(:*)
   end
 
   def even_numeric_elements
-    self.select { |el| el.is_a?(Fixnum) }.select { |num| num % 2 == 0 }
+    select { |el| el.is_a?(Fixnum) }.select { |num| num % 2 == 0 }
   end
 
   def odd_numeric_elements
-    self.select { |el| el.is_a?(Fixnum) }.select { |num| num % 2 == 1 }
+    select { |el| el.is_a?(Fixnum) }.select { |num| num % 2 == 1 }
   end
 
   def string_elements
-    self.select { |el| el.is_a?(String) }
+    select { |el| el.is_a?(String) }
   end
 
   def lowercase_string_elements_as_uppercase
-    self.select { |el| el.is_a?(String) }.select { |str| str == str.downcase }.map { |word| word.capitalize }
+    select { |el| el.is_a?(String) }.select { |str| str == str.downcase }.map { |word| word.capitalize }
   end
 
   def capitalized_elements
-    self.select { |el| el.is_a?(String) }.reject { |term| term[0] == term[0].downcase }
+    select { |el| el.is_a?(String) }.reject { |term| term[0] == term[0].downcase }
   end
 
 end
