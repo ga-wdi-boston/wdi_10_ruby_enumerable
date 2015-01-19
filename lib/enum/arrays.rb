42f2b5e1afbb6528d7b49ce76e21e61ce4d14404
J.Marx <jennyrutledge@gmail.com>
2015-01-19 13:40:33 -0500

    Reformmat classes into appropriate folders, add person_with_most_experience to nested class and test to spec file, and refactored a few methods.

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,24 +1,32 @@
 class Array
 
+  #Sums all Float and Fixnum elements
   def sum_of_numeric_elements
+    select{ |num| num.is_a?(Numeric) }.reduce(:+)
   end
-
+  #Multiplies all Float and Fixnum elements
   def product_of_numeric_elements
+    select{ |num| num.is_a?(Numeric) }.reduce(1, :*)
   end
-
+  #Returns only even Fixnum elements
   def even_numeric_elements
+    select{ |num| num.is_a?(Fixnum) && num.even? }
   end
-
+  #Returns only odd Fixnum elements
   def odd_numeric_elements
+    select{ |num| num.is_a?(Fixnum) && num.odd? }
   end
-
+  #Returns only String elements
   def string_elements
+    select{ |elem| elem.is_a?(String)}
   end
-
+  #Returns String elements that are lowercase in uppercase
   def lowercase_string_elements_as_uppercase
+    select{ |elem| elem.is_a?(String) && elem.match(/\b[a-z]/)}.map(&:capitalize)
   end
-
+  #Returns String elements that are uppercase
   def capitalized_elements
+    select{ |elem| elem.is_a?(String) && elem.match(/\b[A-Z]/)}
   end
 
 end
--- lib/enum/nested.rb
+++ lib/enum/nested.rb
@@ -13,26 +13,34 @@ end
 
 
 class People
+
+  attr_reader :people
   def initialize(people_array)
     @people = people_array
   end
-
+  #Returns a sum of ages from a struct of classes
   def ages_sum
+    people.reduce(0){|sum, info| sum +=info.age }
   end
-
+  #Returns an average of ages from a struct of classes
   def average_age
+    people.reduce(0){|sum, info| sum +=info.age }/people.length
   end
-
+  #Returns the total yr programming  from a struct of classes
   def total_years_programming_experience_for_all_languages
+    people.map{ |info,yrs| info.years_language_experience.values }.flatten!.reduce(:+)
   end
-
+  #Returns a hash of food frequencies from a struct of classes
   def favorite_food_frequency
+    people.map{|info, foodArr| info.favorite_foods }.flatten!.each_with_object(Hash.new(0)){ |key, freq_hash| freq_hash[key] += 1 }
   end
-
-  def total_combined_years_language_experience(language)
-  end
+  #Returns the combined yrs from all languages in a struct of classes
+  def total_combined_years_language_experience(lang)
+     people.map{ |info, years| info.years_language_experience[lang]}.compact.reduce(:+)
+    end
 
   def person_with_most_experience_in_language(language)
+    people.max { |per1,per2| per1.years_language_experience[language].to_i <=> per2.years_language_experience[language].to_i }.name
   end
 
   private
