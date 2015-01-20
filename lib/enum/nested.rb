0be46ad64d8feee5f7235f04f5f74f786e10834d
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-01-19 22:16:13 -0500

    Nested.rb not working

--- lib/enum/nested.rb
+++ lib/enum/nested.rb
@@ -14,25 +14,30 @@ end
 
 class People
   def initialize(people_array)
-    @people = people_array
+    people = people_array
   end
 
   def ages_sum
+    people.reduce{ |sum, age| sum + age}
   end
 
   def average_age
+    people.reduce{ |sum, age| sum + age} /people.length
   end
 
   def total_years_programming_experience_for_all_languages
+    people
   end
 
   def favorite_food_frequency
+    people.each_with_object(Hash.new(0)){ |word, freq| word[freq] +=1}
   end
 
   def total_combined_years_language_experience(language)
   end
 
   def person_with_most_experience_in_language(language)
+    # people.max_by{ |element| element[:years_language_experience](language)}
   end
 
   private
