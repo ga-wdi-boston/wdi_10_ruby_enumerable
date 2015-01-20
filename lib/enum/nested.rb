d81de7bdf6f9410b43d2597adb60dc5b513708c1
KBondanza <kelly.bondanza@gmail.com>
2015-01-19 22:01:37 -0500

    finished the nested.rb section, a little unsure of some of my code. not sure how to find frequency, i tried to count/reduce those numbers and have them injected into a new hash, although i don't think thats quite the right way to approach it.

--- lib/enum/nested.rb
+++ lib/enum/nested.rb
@@ -18,21 +18,27 @@ class People
   end
 
   def ages_sum
+    people.map(&:age).reduce(:+)
   end
 
   def average_age
+    people.map(&:age).reduce(:+) / @people.count
   end
 
   def total_years_programming_experience_for_all_languages
+    people.map{ |people| people.years_language_experience.reduce(:+)}.reduce(:+)
   end
 
   def favorite_food_frequency
+    people.map { |people| people.favorite_foods.count.reduce(:+).object(Hash.new(0))}.inject
   end
 
   def total_combined_years_language_experience(language)
+    people.map { |people| people.years_language_experience.count.reduce(:+)}.reduce(:+)
   end
 
   def person_with_most_experience_in_language(language)
+    people.map { |people| people.years_language_experience.count}.inject
   end
 
   private
