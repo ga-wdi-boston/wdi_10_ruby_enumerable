e3a9701c5f2855d04ef946dc6baee3a348b4475b
CaraGrace <carapacifico@gmail.com>
2015-01-20 08:10:13 -0500

    Basic Enum Homework

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,12 +1,16 @@
+require 'pry'
 class Array
 
   def sum_of_numeric_elements
+    reduce(0) {|sum, element| (element.class == Fixnum || element.class == Float)? sum += element : sum}
   end
 
   def product_of_numeric_elements
+    reduce(1) {|product, element| (element.class == Fixnum || element.class == Float)? product *= element : product}
   end
 
   def even_numeric_elements
+    map {|element| element %2 == 0? element : nil}
   end
 
   def odd_numeric_elements
