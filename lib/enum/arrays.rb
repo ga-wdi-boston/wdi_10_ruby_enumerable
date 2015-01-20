419a2f91bbef38a7295a61421bd1689f78831a3e
lady3bean <annehawleybrett@gmail.com>
2015-01-19 21:31:28 -0500

    passes arrays tests

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,30 +1,31 @@
 class Array
 
-  def initialize(array)
-    @array = array
-  end
-
   def sum_of_numeric_elements
-    sum = @array.reject(String)
-    sum.reduce(:+)
+    select{|num| num.is_a? Numeric}.inject(:+)
   end
 
   def product_of_numeric_elements
+    select{|element| element.is_a? Numeric}.inject(:*)
   end
 
   def even_numeric_elements
+    select{|number| (number.is_a? Fixnum) && (number % 2 == 0)}
   end
 
   def odd_numeric_elements
+    select{|number| (number.is_a? Fixnum) && (number % 2 != 0)}
   end
 
   def string_elements
+    select{|element| element.is_a? String}
   end
 
   def lowercase_string_elements_as_uppercase
+    select{|element| element =~ /\b[a-z]/}.map(&:capitalize)
   end
 
   def capitalized_elements
+    select{|element| element =~ /\b[A-Z]/}
   end
 
 end
